10642e17879058d017c2460ecc4f46c6869b12af
Adman <130444715+Adman0425@users.noreply.github.com>
2025-02-13 14:59:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function loadPool(poolFile) {
         .then(data => {
             cardPool = data.servants.concat(data.craft_essences);
             calculateCardProbabilities(data);
-            console.log("機率計算完畢");
         })
         .catch(error => console.error("載入卡池資料錯誤：", error));
 }
@@ -66,7 +65,7 @@ poolOptions.forEach(option => {
 
 // 初始載入常駐卡池
 loadPool("normal_pool.json");
-console.log("卡池載入");
+// console.log("卡池載入");
 
 // 選擇 HTML 元素
 const singleDrawBtn = document.getElementById("singleDrawBtn");
@@ -109,10 +108,8 @@ function shuffle(array) {
 
 // 單抽按鈕事件監聽器
 singleDrawBtn.addEventListener("click", () => {
-    console.log("單抽已按下");
     resultContainer.innerHTML = "";
     const card = drawCard();
-    console.log("已抽卡");
     if (card) {
         displayCard(card);
     }
