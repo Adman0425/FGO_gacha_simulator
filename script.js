ecff7e149855b36e5796b61a4b4bbdfbe2fcc848
Adman <130444715+Adman0425@users.noreply.github.com>
2025-02-13 11:47:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function displayCard(card) {
     // 常數替代圖片路徑
     const imgSrc = card.imageUrl;
     
-    cardDiv.innerHTML = `<img src="${imgSrc}" width="150" height="150" alt="${card.name}">`;
+    cardDiv.innerHTML = `<img src="${imgSrc}" width="100" height="100" alt="${card.name}">`;
     resultContainer.appendChild(cardDiv);
 }
 
